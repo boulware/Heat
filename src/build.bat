89e600961ea906665198c0a32519b089a968c72a
Tyler Boulware <tyler.boulware@gmail.com>
2015-09-04 19:11:47 -0500

    Link and include freeglut/glew for OpenGL

--- src/build.bat
+++ src/build.bat
@@ -1,9 +1,14 @@
 @echo off
 
-mkdir ..\..\build\heat
-pushd ..\..\build\heat
-cl -Od -FC -Zi -EHsc -MDd ..\..\heat\src\main.cpp -I ../../_tools/SFML-2.3/include -I ../../_tools/xb^
- -link^
- -LIBPATH:../../_tools/SFML-2.3/lib winmm.lib sfml-system-d.lib opengl32.lib gdi32.lib sfml-window-d.lib freetype.lib jpeg.lib sfml-graphics-d.lib
+set PROJECTNAME=heat
+
+mkdir ..\..\build\%PROJECTNAME%
+pushd ..\..\build\%PROJECTNAME%
+cl -O2 -FC -Zi -EHsc -MD ..\..\%PROJECTNAME%\src\main.cpp^
+ -I ../../_tools/SFML-2.3/include^
+ -I ../../_tools/glew-1.13.0/include^
+ -I ../../_tools/freeglut-2.8.1/include^
+ -link -LIBPATH:../../_tools/SFML-2.3/lib^
+ winmm.lib sfml-system.lib opengl32.lib gdi32.lib sfml-window.lib freetype.lib jpeg.lib sfml-graphics.lib glew32.lib
 
 popd
