49d8a4e7c6af6d617c2dfe157b1e6ffae5147f28
Tyler Boulware <tyler.boulware@gmail.com>
2015-09-06 08:07:29 -0500

    Add switches to make debug/release and optimization build switching easier

--- src/build.bat
+++ src/build.bat
@@ -1,16 +1,28 @@
 @echo off
 
+set BUILD=r
+set OPT=-O2
 set PROJECTNAME=heat
+
+IF %BUILD% EQU d (
+   set BUILDSWITCH=-MDd
+   set SUF=-d
+) ELSE (
+  set BUILDSWITCH=-MD
+  set SUF=
+)
+
 
 mkdir ..\..\build\%PROJECTNAME%
 pushd ..\..\build\%PROJECTNAME%
-cl -O2 -FC -Zi -EHsc -MD ..\..\%PROJECTNAME%\src\main.cpp^
+
+cl %OPT% -FC -Zi -EHsc %BUILDSWITCH% ..\..\%PROJECTNAME%\src\main.cpp^
  ../../_tools/_personal/timer.cpp^
  -I ../../_tools/_personal^
  -I ../../_tools/SFML-2.3/include^
  -I ../../_tools/glew-1.13.0/include^
  -I ../../_tools/freeglut-2.8.1/include^
  -link -LIBPATH:../../_tools/SFML-2.3/lib^
- winmm.lib sfml-system.lib opengl32.lib gdi32.lib sfml-window.lib freetype.lib jpeg.lib sfml-graphics.lib glew32.lib
+ winmm.lib sfml-system%SUF%.lib opengl32.lib gdi32.lib sfml-window%SUF%.lib freetype.lib jpeg.lib sfml-graphics%SUF%.lib glew32.lib
 
 popd
